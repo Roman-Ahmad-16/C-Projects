fcb398f1b9e8b7048aa00eb4167398991dbaaf2c
Roman-Ahmad-16 <websitedesigner725@gmail.com>
2025-07-09 22:29:08 +0500

    find

---
+++ Jagged Array/find.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	const int rows=3;
+	const int cols=3;
+	
+	int array[rows][cols];
+	
+	cout<<"ENTER VALUES :: "<<endl;
+	
+	for(int i=0; i<rows; i++){
+		for(int j=0; j<cols; j++){
+			cin>>array[i][j];
+		}
+	}
+	
+	cout<<"VALUES OF 2D ARRAY :: "<<endl;
+	
+	for(int i=0; i<rows; i++){
+		for(int j=0; j<cols; j++){
+			cout<<array[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	
+	int compare=array[0][0];
+	
+	cout << "\nComparing first value (" << compare << ") with 2nd and 3rd row...\n";
+
+	for(int i=1; i<rows; i++){
+		for(int j=0; j<cols; j++){
+			if(array[i][j]==compare){
+				cout<<"VALUE :: "<<array[i][j]<<" MATCH AT THE INDEX (" << i << "," << j << ")\n"; 
+			}
+		}
+	}
+	return 0;
+}
