5cedaf69661e9fbe2912e52b6f240fa0508db4eb
Roman-Ahmad-16 <websitedesigner725@gmail.com>
2025-07-11 23:02:16 +0500

    2D

--- Arrays/Value _ Index.cpp
+++ Arrays/Value _ Index.cpp
@@ -4,7 +4,7 @@ int main(){
 	
 	
 	int arr[6]={10,20,30,40,50,60};
-	int n,loc=-1;
+	int n,loc=-2;
 	
 	
 	cout<<"ENTER VALUE TO FIND = ";
@@ -14,7 +14,7 @@ int main(){
 	for(int i=0; i<6; i++){
 		if(arr[i]==n){
 			loc=i;
-			if(loc==-1){
+			if(loc==-2){
 				cout<<"VALUE NOT FOUND IN THE ARRAY"<<endl;
 			}else{
 			cout<<"VALUE FOUND AT THE INDEX:"<<loc<<endl;
