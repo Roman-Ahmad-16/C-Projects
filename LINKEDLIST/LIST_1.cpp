99e0c070ecbfbf150bcff248e502afe2b5780c97
Roman-Ahmad-16 <websitedesigner725@gmail.com>
2026-05-13 23:51:48 +0500

    ll

--- LINKEDLIST/LIST_1.cpp
+++ LINKEDLIST/LIST_1.cpp
@@ -34,6 +34,75 @@ public:
 		}
 	}
 	
+	void push_back(int val){
+		Node* newNode = new Node(val);
+		
+		if(head==NULL)
+		{
+			head = tail = newNode;
+		}else {
+			tail->next = newNode;
+			tail = newNode;
+		}
+	}
+	
+	void pop_front(){
+		if(head == NULL){
+			cout<<"LL is empty\n";
+			return;
+		}
+		
+		Node* temp = head;
+		head = head->next;
+		temp->next = NULL;
+		
+		delete temp;
+	}
+	
+	void pop_back(){
+		if(head == NULL)
+		{
+			cout<<"LL is empty\n";
+			return;
+		}
+		
+		Node* temp = head;
+		while(temp->next!=tail)
+		{
+			temp = temp->next;
+		}
+		
+		temp->next = NULL;
+		delete tail;
+		tail = temp;
+	}
+	
+	void insert(int val , int pos ){
+		if(pos < 0) {
+			cout<<"Invalid pos\n";
+			return;
+		}
+		
+		if(pos == 0){
+			push_front(val);
+			return;
+		}
+		
+		Node* temp = head;
+		for(int i=0; i<pos-1; i++)
+		{
+			if(temp == NULL ){
+				cout<<"Invalid pos\n";
+				return;
+			}
+			temp = temp->next;
+		}
+		
+		Node* newNode = new Node(val);
+		newNode->next = temp->next;
+		temp->next = newNode;
+	}
+	
 	void printLL(){
 		Node* temp = head;
 		
@@ -43,15 +112,48 @@ public:
 		}
 		cout<<"NULL"<<endl;
 	}
+	
+	int search(int key) {
+		Node* temp = head;
+		int idx = 0;
+		
+		while(temp != NULL)
+		{
+			if(temp->data == key){
+				return idx;
+			}
+			
+			temp = temp->next;
+			idx++;
+		}
+		return -1;
+	}
 };
 int main(){
 	
 	List ll;
 	
-	ll.push_front(1);
-	ll.push_front(2);
 	ll.push_front(3);
+	ll.push_front(2);
+	ll.push_front(1);
+	
+//	ll.printLL();
+//	
+//	ll.push_back(4);
+//	
+//	ll.printLL();
+//	
+//	ll.pop_front();
+//	
+//	ll.printLL();
+//	
+//	ll.pop_back();
+	
+	ll.insert(4,0);
 	
 	ll.printLL();
+	
+	cout<<ll.search(2)<<endl;
+	
 	return 0;
 }
